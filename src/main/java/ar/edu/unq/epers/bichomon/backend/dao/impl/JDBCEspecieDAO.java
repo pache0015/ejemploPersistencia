b7c8206c4cc6069e76c21b8b1f67bf4edc153dd2
Lucas Wojciechow <lucas.wojciechow@10pines.com>
2019-09-06 18:09:01 -0300

    refactor: encapsulando codigo que utiliza PreparedStatement y ResultSet en metodos

--- src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/JDBCEspecieDAO.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/JDBCEspecieDAO.java
@@ -25,11 +25,7 @@ public class JDBCEspecieDAO implements EspecieDAO {
         try {
             this.executeWithConnection(conn -> {
                 PreparedStatement ps = conn.prepareStatement("INSERT INTO especie (nombre, peso, altura, tipo, cantidad_de_bichos) VALUES (?,?,?,?,?)");
-                ps.setString(1, especie.getNombre());
-                ps.setInt(2, especie.getPeso());
-                ps.setInt(3, especie.getAltura());
-                ps.setString(4, especie.getTipo().toString());
-                ps.setInt(5, especie.getCantidadBichos());
+                this.setearEspecie(especie, ps);
 
                 ps.execute();
                 ps.close();
@@ -47,11 +43,7 @@ public class JDBCEspecieDAO implements EspecieDAO {
         try {
             this.executeWithConnection(conn -> {
                 PreparedStatement ps = conn.prepareStatement("UPDATE especie SET nombre = ?, peso = ?, altura = ?, tipo = ?, cantidad_de_bichos = ?");
-                ps.setString(1, especie.getNombre());
-                ps.setInt(2, especie.getPeso());
-                ps.setInt(3, especie.getAltura());
-                ps.setString(4, especie.getTipo().toString());
-                ps.setInt(5, especie.getCantidadBichos());
+                this.setearEspecie(especie, ps);
 
                 ps.execute();
                 ps.close();
@@ -73,10 +65,7 @@ public class JDBCEspecieDAO implements EspecieDAO {
 
             Especie especie = null;
             while (resultSet.next()) {
-                especie = new Especie(resultSet.getInt("id"), resultSet.getString("nombre"), TipoBicho.valueOf(resultSet.getString("tipo")));
-                especie.setAltura(resultSet.getInt("altura"));
-                especie.setCantidadBichos(resultSet.getInt("cantidad_de_bichos"));
-                especie.setPeso(resultSet.getInt("peso"));
+                especie = this.getEspecieResultante(resultSet);
             }
 
             ps.close();
@@ -92,12 +81,7 @@ public class JDBCEspecieDAO implements EspecieDAO {
             List<Especie> especiesRecuperadas = new ArrayList<>();
 
             while (resultSet.next()) {
-
-                Especie nuevaEspecieRecuperada = new Especie(resultSet.getInt("id"), resultSet.getString("nombre"), TipoBicho.valueOf(resultSet.getString("tipo")));
-                nuevaEspecieRecuperada.setAltura(resultSet.getInt("altura"));
-                nuevaEspecieRecuperada.setCantidadBichos(resultSet.getInt("cantidad_de_bichos"));
-                nuevaEspecieRecuperada.setPeso(resultSet.getInt("peso"));
-
+                Especie nuevaEspecieRecuperada = this.getEspecieResultante(resultSet);
                 especiesRecuperadas.add(nuevaEspecieRecuperada);
 
             }
@@ -105,6 +89,23 @@ public class JDBCEspecieDAO implements EspecieDAO {
             ps.close();
             return especiesRecuperadas;
         });
+    }
+
+    private Especie getEspecieResultante(ResultSet resultSet) throws SQLException {
+        Especie especie = new Especie(resultSet.getInt("id"), resultSet.getString("nombre"), TipoBicho.valueOf(resultSet.getString("tipo")));
+        especie.setAltura(resultSet.getInt("altura"));
+        especie.setCantidadBichos(resultSet.getInt("cantidad_de_bichos"));
+        especie.setPeso(resultSet.getInt("peso"));
+
+        return especie;
+    }
+
+    private void setearEspecie(Especie especie, PreparedStatement ps) throws SQLException {
+        ps.setString(1, especie.getNombre());
+        ps.setInt(2, especie.getPeso());
+        ps.setInt(3, especie.getAltura());
+        ps.setString(4, especie.getTipo().toString());
+        ps.setInt(5, especie.getCantidadBichos());
     }
 
     /**
