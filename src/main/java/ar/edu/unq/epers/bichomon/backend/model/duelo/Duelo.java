d62c919ca5264a8bf2babbc8990c49e051887403
pache0015 <pache.facundo@gmail.com>
2019-09-22 20:27:03 -0300

    Duelo in progress

--- src/main/java/ar/edu/unq/epers/bichomon/backend/model/duelo/Duelo.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/model/duelo/Duelo.java
@@ -3,18 +3,28 @@ package ar.edu.unq.epers.bichomon.backend.model.duelo;
 import ar.edu.unq.epers.bichomon.backend.model.bicho.Bicho;
 import ar.edu.unq.epers.bichomon.backend.model.entrenador.Entrenador;
 
-import static java.lang.Math.random;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Duelo {
     private Entrenador retador;
     private Entrenador campeon;
     private Integer cantAtaques;
+    private Entrenador nuevoCampeon;
+    private List<Ataque> ataques;
+    private Bicho atacante;
+    private Bicho atacado;
+    private Bicho auxiliar;
 
 
     public Duelo(Entrenador retador, Entrenador campeon){
         this.retador = retador;
         this.campeon = campeon;
-        this.cantAtaques = 0;
+        this.nuevoCampeon = null;
+        this.ataques = new ArrayList<>();
+        this.atacante = this.retador.getBichoParaDuelo();
+        this.atacado = this.campeon.getBichoParaDuelo();
     }
 
     private void atacar(Bicho atacante, Bicho rival){
@@ -24,7 +34,12 @@ public class Duelo {
 
         rival.reducirEnergia(this.energiaAQuitar(energiaAtacante));
 
-        cantAtaques ++;
+
+        this.cargarAtaque(atacante, rival, energiaAtacante);
+    }
+
+    private void cargarAtaque(Bicho atacante, Bicho rival, Double energiaAtacante) {
+        this.ataques.add((new Ataque(atacante, rival, energiaAtacante)));
     }
 
     private Double energiaAQuitar(Double valor){
@@ -34,16 +49,21 @@ public class Duelo {
 
     public void atacarSiValida(Entrenador atacante, Entrenador rival){
 
-        if (atacante.getBichoParaDuelo().puedeSeguir()){
+        if (atacante.getBichoParaDuelo().puedeSeguir() && (ataques.size() < 10)){
             this.atacar(atacante.getBichoParaDuelo(), rival.getBichoParaDuelo());
         }
-        else {
+        else{
             this.finalizar();
-
         }
     }
 
-    private void finalizar() {
-        
+    private ResultadoCombate finalizar() {
+        return new ResultadoCombate(nuevoCampeon, nuevoCampeon.getBichoParaDuelo(), ataques);
+    }
+
+
+    private void setearAtacanteYAtacado(Bicho bicho1, Bicho bicho2){
+
+
     }
 }
