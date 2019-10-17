70a05a7b21be9b6c0566dbb167b8202192511dc7
Lucas Wojciechow <lucas.wojciechow@10pines.com>
2019-10-17 19:37:04 -0300

    borrando ExperienciaService y ExperienciaDao inutilizados

--- src/main/java/ar/edu/unq/epers/bichomon/backend/jdbc/service/experiencia/ExperienciaDao.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.epers.bichomon.backend.jdbc.service.experiencia;
-
-import ar.edu.unq.epers.bichomon.backend.model.entrenador.Experiencia;
-
-public interface ExperienciaDao {
-
-    Experiencia recuperar(Long id);
-    void guardar(Experiencia experiencia);
-}
